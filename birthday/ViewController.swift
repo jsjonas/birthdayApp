7ae92433bc39dca5ab97a8ec17c81e2729f56e17
monalisa <yona@marketing66.com>
2021-09-22 21:26:58 +0200

    Initial Commit

---
+++ birthday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  birthday
+//
+//  Created by Jonas S on 22/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
