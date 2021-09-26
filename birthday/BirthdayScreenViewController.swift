273a1145dbe3cbe2c994360fe31cf320a3b2958e
monalisa <yona@marketing66.com>
2021-09-26 08:21:17 +0200

    logos and images added random function setted camera icon setted share function setted

--- birthday/BirthdayScreenViewController.swift
+++ birthday/BirthdayScreenViewController.swift
@@ -8,37 +8,135 @@
 import UIKit
 
 class BirthdayScreenViewController: UIViewController {
-
+    
     @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var babyImage: UIImageView!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var shareButton: UIButton!
+    
+    var cameraIcon: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-//        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "iOsBgElephant")!)
-        setTheme(themeNumber: 1)
+        setPhotoIcon()
+        
+        //        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "iOsBgElephant")!)
+        let randomInt = Int.random(in: 1..<4)
+        
+        setTheme(themeNumber: randomInt)
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //        babyImage.layer.cornerRadius = 0.5 * babyImage.frame.height
+        
+        cameraIcon.layoutIfNeeded()
+        cameraIcon.layer.cornerRadius = 0.5 * cameraIcon.frame.height
+    }
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func shareScreenshot(_ sender: Any) {
+        
+        setItemHidden(isHidden: true)
+        //Create the UIImage
+        let renderer = UIGraphicsImageRenderer(size: view.frame.size)
+        let image = renderer.image(actions: { context in
+            view.layer.render(in: context.cgContext)
+        })
+        
+        //Save it to the camera roll
+        //        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        
+        setItemHidden(isHidden: false)
+        
+        var imagesToShare = [AnyObject]()
+        imagesToShare.append(image)
+        
+        let activityViewController = UIActivityViewController(activityItems: imagesToShare , applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        present(activityViewController, animated: true, completion: nil)
+        
+    }
+    
+    func setItemHidden(isHidden: Bool) -> Void {
+        backButton.isHidden = isHidden
+        shareButton.isHidden = isHidden
+        cameraIcon.isHidden = isHidden
+    }
+    
+    func setPhotoIcon() -> Void {
+        cameraIcon = UIImageView()
+        cameraIcon.translatesAutoresizingMaskIntoConstraints = false
+        cameraIcon.backgroundColor = .green
+        babyImage.addSubview(cameraIcon)
+        
+        cameraIcon.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        cameraIcon.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        
+        let (hMult, vMult) = computeMultipliers(angle: 45)
+        
+        // position the little green circle using a multiplier on the right and bottom
+        NSLayoutConstraint(item: cameraIcon!, attribute: .centerX, relatedBy: .equal, toItem: babyImage!, attribute: .trailing, multiplier: hMult, constant: 0).isActive = true
+        NSLayoutConstraint(item: cameraIcon!, attribute: .centerY, relatedBy: .equal, toItem: babyImage!, attribute: .bottom, multiplier: vMult, constant: 0).isActive = true
+        
+    }
+    
+    func computeMultipliers(angle: CGFloat) -> (CGFloat, CGFloat) {
+        let radians = angle * .pi / 180
+        
+        let h = (1.0 + cos(radians)) / 2
+        let v = (1.0 - sin(radians)) / 2
+        
+        return (h, v)
     }
     
     func setTheme(themeNumber: Int) -> Void {
         switch themeNumber {
         case 1:
             backgroundImage.image = UIImage(named: "iOsBgElephant")
+            babyImage.image = UIImage(named: "defaultPlaceHolderYellow")
+            cameraIcon.image = UIImage(named: "cameraIconYellow")
+            
+            
+            break
+        case 2:
+            backgroundImage.image = UIImage(named: "iOsBgFox")
+            babyImage.image = UIImage(named: "defaultPlaceHolderGreen")
+            cameraIcon.image = UIImage(named: "cameraIconGreen")
+            
+            
+            break
+        case 3:
+            backgroundImage.image = UIImage(named: "iOsBgPelican2")
+            babyImage.image = UIImage(named: "defaultPlaceHolderBlue")
+            cameraIcon.image = UIImage(named: "cameraIconBlue")
+            
+            
+            
             break
         default:
             break
         }
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- birthday/SetDetailsViewController.swift
+++ birthday/SetDetailsViewController.swift
@@ -8,20 +8,49 @@
 import UIKit
 
 class SetDetailsViewController: UIViewController {
-
+    
     @IBOutlet weak var profileImage: UIImageView!
     
     @IBOutlet weak var pictureButton: UIButton!
     
+    @IBOutlet weak var birthDate: UIDatePicker!
+    
+    @IBOutlet weak var name: UITextField!
+    
     var imagePicker = UIImagePickerController()
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: Date())
+        //        print(tomorrow)
+        birthDate.date = tomorrow!
+        
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    @IBAction func showBirthdayScreenBtn(_ sender: Any) {
+        
+        if name.hasText && birthDate.date < Date() {
+            performSegue(withIdentifier: "fromSetDetailsSegue", sender: self)
+            
+        } else {
+            let alert = UIAlertController(title: "Empty Fields", message: "Please fill the baby name and picture", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil
+            ))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        
+        
+    }
+    
     @IBAction func takePictureBtn(_ sender: Any) {
         
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -36,11 +65,12 @@ class SetDetailsViewController: UIViewController {
         alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
         
         self.present(alert, animated: true, completion: nil)
-
+        
         
     }
     
-    func openCamera(){
+    func openCamera() {
+        
         if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera)){
             imagePicker.sourceType = UIImagePickerController.SourceType.camera
             //If you dont want to edit the photo then you can set allowsEditing to false
@@ -54,13 +84,23 @@ class SetDetailsViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
         }
     }
+    
+    func openGallery() {
         
-    func openGallery(){
         imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
         //If you dont want to edit the photo then you can set allowsEditing to false
         imagePicker.allowsEditing = true
         imagePicker.delegate = self
         self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? BirthdayScreenViewController {
+            //            vc?.date = self
+            //            vc?.pic = self
+            //            vc?.name = self
+            
+        }
     }
 }
 
